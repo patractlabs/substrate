0ec0575646bcfe3ac39c7f8afdbfebc0b4a0c420
clearloop <udtrokia@gmail.com>
2020-12-10 10:56:14 +0800

    feat(io): adapt the new api of megaclite

--- bin/node-template/io/src/lib.rs
+++ bin/node-template/io/src/lib.rs
@@ -7,82 +7,66 @@ use sp_runtime_interface::runtime_interface;
 #[runtime_interface]
 pub trait ZkSnarks {
     fn bls12_377_add() {
-        megaclite::tests::bls12_377_add();
+        curve::tests::add(0x2a);
     }
 
     fn bls12_377_mul() {
-        megaclite::tests::bls12_377_mul();
+        curve::tests::mul(0x2a);
     }
 
     fn bls12_377_pairing_two() {
-        megaclite::tests::bls12_377_pairing();
+        curve::tests::pairing(0x2a);
     }
 
     fn bls12_377_pairing_six() {
-        megaclite::tests::bls12_377_pairing_six();
+        curve::tests::pairing_six(0x2a);
     }
 
     fn bls12_381_add() {
-        megaclite::tests::bls12_381_add();
+        curve::tests::add(0x2b);
     }
 
     fn bls12_381_mul() {
-        megaclite::tests::bls12_381_mul();
+        curve::tests::mul(0x2b);
     }
 
     fn bls12_381_pairing_two() {
-        megaclite::tests::bls12_381_pairing();
+        curve::tests::pairing(0x2b);
     }
 
     fn bls12_381_pairing_six() {
-        megaclite::tests::bls12_381_pairing_six();
+        curve::tests::pairing_six(0x2b);
     }
 
     fn bn254_add() {
-        megaclite::tests::bn254_add();
+        curve::tests::add(0x2c);
     }
 
     fn bn254_mul() {
-        megaclite::tests::bn254_mul();
+        curve::tests::mul(0x2c);
     }
 
     fn bn254_pairing_two() {
-        megaclite::tests::bn254_pairing();
+        curve::tests::pairing(0x2c);
     }
 
     fn bn254_pairing_six() {
-        megaclite::tests::bn254_pairing_six();
+        curve::tests::pairing_six(0x2c);
     }
 
     fn bw6_761_add() {
-        megaclite::tests::bw6_761_add();
+        curve::tests::add(0x2d);
     }
 
     fn bw6_761_mul() {
-        megaclite::tests::bw6_761_mul();
+        curve::tests::mul(0x2d);
     }
 
     fn bw6_761_pairing_two() {
-        megaclite::tests::bw6_761_pairing();
+        curve::tests::pairing(0x2d);
     }
 
     fn bw6_761_pairing_six() {
-        megaclite::tests::bw6_761_pairing_six();
-    }
-
-    fn cp6_782_add() {
-        megaclite::tests::cp6_782_add();
-    }
-
-    fn cp6_782_mul() {
-        megaclite::tests::cp6_782_mul();
-    }
-
-    fn cp6_782_pairing_two() {
-        megaclite::tests::cp6_782_pairing();
-    }
-
-    fn cp6_782_pairing_six() {
-        megaclite::tests::cp6_782_pairing_six();
+        curve::tests::pairing_six(0x2d);
     }
 }
